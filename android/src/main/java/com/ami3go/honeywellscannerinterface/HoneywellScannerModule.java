a59742a5c4e8cfb94012efb48be2ddd86da8d9a6
Damian Molinaro <damian.molinaro@gmail.com>
2025-12-10 16:56:24 -0300

    Initial

---
+++ android/src/main/java/com/ami3go/honeywellscannerinterface/HoneywellScannerModule.java
@@ -0,0 +1,158 @@
+package com.ami3go.honeywellscannerinterface;
+
+import android.os.Build;
+import android.util.Log;
+
+import com.facebook.react.bridge.*;
+import com.facebook.react.modules.core.DeviceEventManagerModule;
+import com.honeywell.aidc.*;
+import com.honeywell.aidc.AidcManager.CreatedCallback;
+
+import javax.annotation.Nullable;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static com.ami3go.honeywellscannerinterface.HoneywellScannerPackage.HoneyWellTAG;
+
+public class HoneywellScannerModule extends ReactContextBaseJavaModule implements BarcodeReader.BarcodeListener {
+
+    // Debugging
+    private static final boolean D = true;
+
+    private final ReactApplicationContext reactContext;
+    private AidcManager manager;
+    private BarcodeReader reader;
+    private boolean isReaderActive = false;
+
+    private static final String BARCODE_READ_SUCCESS = "barcodeReadSuccess";
+    private static final String BARCODE_READ_FAIL = "barcodeReadFail";
+
+    public HoneywellScannerModule(ReactApplicationContext reactContext) {
+        super(reactContext);
+        this.reactContext = reactContext;
+    }
+
+    @Override
+    public String getName() {
+        return "HoneywellScanner";
+    }
+
+    /**
+     * Send event to javascript
+     *
+     * @param eventName Name of the event
+     * @param params    Additional params
+     */
+    private void sendEvent(String eventName, @Nullable WritableMap params) {
+        if (reactContext.hasActiveCatalystInstance()) {
+            if (D) Log.d(HoneyWellTAG, "Sending event: " + eventName);
+            reactContext
+                    .getJSModule(DeviceEventManagerModule.RCTDeviceEventEmitter.class)
+                    .emit(eventName, params);
+        }
+    }
+
+    @Override
+    public void onBarcodeEvent(BarcodeReadEvent barcodeReadEvent) {
+        if (D) Log.d(HoneyWellTAG, "HoneywellBarcodeReader - Barcode scan read");
+        WritableMap params = Arguments.createMap();
+        params.putString("data", barcodeReadEvent.getBarcodeData());
+        sendEvent(BARCODE_READ_SUCCESS, params);
+    }
+
+    @Override
+    public void onFailureEvent(BarcodeFailureEvent barcodeFailureEvent) {
+        if (D) Log.d(HoneyWellTAG, "HoneywellBarcodeReader - Barcode scan failed");
+        sendEvent(BARCODE_READ_FAIL, null);
+    }
+
+    /*******************************/
+    /** Methods Available from JS **/
+    /*******************************/
+
+    @ReactMethod
+    public void startReader(final Promise promise) {
+        // Si ya hay un reader activo, no crear uno nuevo
+        if (isReaderActive && reader != null) {
+            Log.w(HoneyWellTAG, "Reader already active, skipping initialization");
+            promise.resolve(true);
+            return;
+        }
+
+        AidcManager.create(reactContext, new CreatedCallback() {
+            @Override
+            public void onCreated(AidcManager aidcManager) {
+                manager = aidcManager;
+                reader = manager.createBarcodeReader();
+                if (reader != null) {
+                    reader.addBarcodeListener(HoneywellScannerModule.this);
+                    try {
+                        reader.claim();
+                        reader.setProperty(BarcodeReader.PROPERTY_EAN_8_ENABLED, true);
+                        reader.setProperty(BarcodeReader.PROPERTY_EAN_8_CHECK_DIGIT_TRANSMIT_ENABLED, true);
+                        reader.setProperty(BarcodeReader.PROPERTY_EAN_13_ENABLED, true);
+                        reader.setProperty(BarcodeReader.PROPERTY_EAN_13_CHECK_DIGIT_TRANSMIT_ENABLED, true);
+                        reader.setProperty(BarcodeReader.PROPERTY_EAN_13_TWO_CHAR_ADDENDA_ENABLED, true);
+                        reader.setProperty(BarcodeReader.PROPERTY_EAN_13_FIVE_CHAR_ADDENDA_ENABLED, true);
+                        isReaderActive = true;
+                        promise.resolve(true);
+                    } catch (ScannerUnavailableException | UnsupportedPropertyException e) {
+                        Log.e(HoneyWellTAG, "Error setting scanner properties: " + e.getMessage());
+                        isReaderActive = false;
+                        promise.resolve(false);
+                        e.printStackTrace();
+                    } catch (IllegalStateException e) {
+                        Log.e(HoneyWellTAG, "BarcodeReader is closed or in invalid state: " + e.getMessage());
+                        isReaderActive = false;
+                        promise.resolve(false);
+                        e.printStackTrace();
+                    } catch (Exception e) {
+                        Log.e(HoneyWellTAG, "Unexpected error starting reader: " + e.getMessage());
+                        isReaderActive = false;
+                        promise.resolve(false);
+                        e.printStackTrace();
+                    }
+                } else {
+                    Log.e(HoneyWellTAG, "Failed to create BarcodeReader");
+                    promise.resolve(false);
+                }
+            }
+        });
+    }
+
+    @ReactMethod
+    public void stopReader(Promise promise) {
+        try {
+                if (reader != null) {
+                    isReaderActive = false;
+                    reader.close();
+                }
+                reader = null;
+                if (manager != null) {
+                    manager.close();
+                }
+                manager = null;
+                promise.resolve(null);
+            } catch (Exception e) {
+                Log.e(HoneyWellTAG, "Error in stopReader: " + e.getMessage());
+                isReaderActive = false;
+                promise.resolve(null); // Resolver de todas formas para no bloquear JS
+            }
+    }
+
+    private boolean isCompatible() {
+        // This... is not optimal. Need to find a better way to performantly check whether device has a Honeywell scanner
+        return Build.BRAND.toLowerCase().contains("honeywell");
+    }
+
+    @Override
+    public Map<String, Object> getConstants() {
+        final Map<String, Object> constants = new HashMap<>();
+        constants.put("BARCODE_READ_SUCCESS", BARCODE_READ_SUCCESS);
+        constants.put("BARCODE_READ_FAIL", BARCODE_READ_FAIL);
+        constants.put("isCompatible", isCompatible());
+        return constants;
+    }
+
+}
